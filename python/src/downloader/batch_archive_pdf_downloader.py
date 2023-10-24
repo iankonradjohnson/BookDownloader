a60990d82ab785b9e821d5100dccf3018975a1e5
iankonradjohnson <iankonradjohnson@gmail.com>
2023-10-23 20:52:21 -0400

    Adding tests and other stuff

--- python/src/downloader/batch_archive_pdf_downloader.py
+++ python/src/downloader/batch_archive_pdf_downloader.py
@@ -1,71 +1,79 @@
 import os
-import shutil
-import zipfile
-from io import BytesIO
-import requests
-from PIL import Image
-from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
+
+import requests
+from tqdm import tqdm
+
 from python.src.downloader.downloader import Downloader
 
-MAX_DOWNLOAD_WORKERS = 10
+PROCESSED_ZIP = "PROCESSED_ZIP"
+ORIGINAL_TAR = "ORIGINAL_TAR"
 
+MAX_DOWNLOAD_WORKERS = 5
 
-class BatchArchivePdfDownloader(Downloader):
+# This file is depricated, to download in bulk from archive please see the following
+# website: https://blog.archive.org/2012/04/26/downloading-in-bulk-using-wget/
 
-    def __init__(self, config):
+class BatchArchiveDownloader(Downloader):
+
+    def __init__(self, config, download_type="PROCESSED_ZIP"):
         self.input_list = config.get("input_list")
         self.save_folder = config.get("output_path")
+        self.download_type = download_type
 
-    def process_file(self, file_name, extracted_folder):
-        try:
-            if file_name.endswith(".jp2"):
-                file_path = os.path.join(extracted_folder, file_name)
-                with Image.open(file_path) as image:
-                    png_file_name = os.path.splitext(file_name)[0] + ".png"
-                    png_path = os.path.join(self.save_folder, png_file_name)
-                    image.save(png_path, format="PNG")
-                os.remove(file_path)  # remove the original .jp2 file after conversion
-        except Exception as e:
-            print(f"Error at page level on {file_name}: {e}")
-
-    def download_and_process_url(self, url):
+    def download_and_process_url(self, identifier):
         try:
             with requests.Session() as session:
-                # Extract the book identifier from the URL
-                identifier = url.split("/")[-5]
-                download_url = f"https://archive.org/download/{identifier}/{identifier}_jp2.zip"
+                identifier = str.strip(identifier)
 
-                # Download the zip file and extract the JP2 images
-                response = session.get(download_url)
-                zip_file = BytesIO(response.content)
-                extracted_folder = os.path.join(self.save_folder, identifier + "_jp2")
+                # Get Archive identifier
+                filename = self.create_filename(identifier, self.download_type)
+                download_url = self.create_url(filename, identifier)
+                save_path = os.path.join(self.save_folder, filename)
 
-                with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-                    zip_ref.extractall(self.save_folder)
+                if not os.path.exists(save_path):
 
-                files_in_folder = os.listdir(extracted_folder)
-                if not files_in_folder:  # check if the folder is empty
-                    print(f"No files found in {extracted_folder}")
-                    return
+                    # Binary Download
+                    response = session.get(download_url)
 
-                # Parallelize the processing part for each downloaded content
-                with ThreadPoolExecutor() as executor:
-                    list(tqdm(executor.map(self.process_file, files_in_folder,
-                                           [extracted_folder] * len(files_in_folder)),
-                              desc="Converting Images"))
+                    if response.status_code == 404:
+                        filename = self.create_filename(identifier, PROCESSED_ZIP)
+                        download_url = self.create_url(
+                            filename,
+                            identifier)
+                        response = session.get(download_url)
 
-                # You can remove this line if you want to keep the extracted folder intact
-                # shutil.rmtree(extracted_folder)
+                    # Write the content to the file
+                    with open(save_path, 'wb') as fd:
+                        for chunk in response.iter_content(chunk_size=8192):
+                            fd.write(chunk)
 
         except Exception as e:
             print(f"Error in download_and_process_url: {e}")
 
+    def create_url(self, filename, identifier):
+        download_url = f"https://archive.org/download/{identifier}/{filename}"
+        return download_url
+
+    def create_filename(self, identifier, download_type):
+
+        if download_type == PROCESSED_ZIP:
+            return f"{identifier}_jp2.zip"
+        elif download_type == ORIGINAL_TAR:
+            return f"{identifier}_orig_jp2.tar"
+        else:
+            raise NotImplemented()
+
+    # Batch Downloader
     def download(self):
         # Open the text file containing the image paths
         with open(self.input_list, 'r') as f:
-            urls = list(tqdm(f, desc="Fetching URLs"))
+            identifiers = list(tqdm(f, desc="Fetching URLs"))
 
-            with ThreadPoolExecutor() as executor:
-                list(tqdm(executor.map(self.download_and_process_url, urls), desc="Downloading and Processing",
-                          total=len(urls)))
+            with ThreadPoolExecutor(max_workers=MAX_DOWNLOAD_WORKERS) as executor:
+                tqdm(
+                    executor.map(
+                        self.download_and_process_url,
+                        identifiers),
+                    desc="Downloading and Processing",
+                    total=len(identifiers))
